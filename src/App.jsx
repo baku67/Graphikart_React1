12a464c484df1d6834286c39fa78c842cdbe14a7
baku67 <basile08@hotmail.fr>
2023-11-02 14:35:39 +0100

    HOOK: useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,40 @@ import { Fragment } from 'react'
 
 function App() {
 
+  const [count, setCount] = useState(0)
+
+  const countIncr = () => {
+    setCount(count + 1)
+  }
+
+
+
+  let [person, setPerson] = useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 18,
+  })
+
+  // Pas de "mutation" en REACT
+  const ageIncr = () => {
+    // "récupère toutes les propriétés de l'objet person original, et change la valeur de age"
+    setPerson({...person, age: person.age +1})
+  }
+
+
+  
   return (
-    <h1>useState</h1>
+    <Fragment>
+      <h1>HOOK: useState</h1>
+
+      <p>Compteur: {count}</p>
+      <button onClick={countIncr}>Incrémenter le compteur</button>
+
+
+      <p>Personne: {person.firstName} {person.lastName} ({person.age}ans)</p>
+      <button onClick={ageIncr}>+1 an</button> 
+
+    </Fragment>
   )
 }
 
